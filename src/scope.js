d4661a0e42ef649268478ea27a2db8763f028e57
lijsh <sysulijs@gmail.com>
2016-08-06 22:43:19 +0800

    about initial watch value

--- src/scope.js
+++ src/scope.js
@@ -1,17 +1,29 @@
 import _ from 'lodash'
 
+function initWatchVal() {}
+
 module.exports = class Scope {
   constructor() {
     this.$$watchers = []
   }
   $watch(watchFn, listenerFn) {
-    const watcher = { watchFn, listenerFn }
+    const watcher = {
+      watchFn,
+      listenerFn: listenerFn || function () {},
+      last: initWatchVal,
+    }
     this.$$watchers.push(watcher)
   }
   $digest() {
+    let newValue
+    let oldValue
     _.forEach(this.$$watchers, watcher => {
-      watcher.watchFn(this)
-      watcher.listenerFn()
+      newValue = watcher.watchFn(this)
+      oldValue = watcher.last
+      if (newValue !== oldValue) {
+        watcher.last = newValue
+        watcher.listenerFn(newValue, (oldValue === initWatchVal ? newValue : oldValue), this)
+      }
     })
   }
 }
--- test/scope_spec.js
+++ test/scope_spec.js
@@ -2,35 +2,88 @@ const Scope = require('../src/scope.js')
 
 describe('Scope', () => {
   it('can be constructed and used as an object', () => {
-    const scope = new Scope()
-    scope.aProperty = 1
+    const $scope = new Scope()
+    $scope.aProperty = 1
 
-    expect(scope.aProperty).toBe(1)
+    expect($scope.aProperty).toBe(1)
   })
 
   describe('digest', () => {
-    let scope
+    let $scope
     beforeEach(() => {
-      scope = new Scope()
+      $scope = new Scope()
     })
 
     it('calls the listener function of a watch on first $digest', () => {
       const watchFn = () => 'wat'
       const listenerFn = jasmine.createSpy()
-      scope.$watch(watchFn, listenerFn)
+      $scope.$watch(watchFn, listenerFn)
 
-      scope.$digest()
+      $scope.$digest()
       expect(listenerFn).toHaveBeenCalled()
     })
 
     it('calls the watch function with the scope as the argument', () => {
       const watchFn = jasmine.createSpy()
       const listenerFn = () => {}
-      scope.$watch(watchFn, listenerFn)
+      $scope.$watch(watchFn, listenerFn)
 
-      scope.$digest()
+      $scope.$digest()
 
-      expect(watchFn).toHaveBeenCalledWith(scope)
+      expect(watchFn).toHaveBeenCalledWith($scope)
+    })
+
+    it('calls the listener function when the watched value changes', () => {
+      $scope.someValue = 'a'
+      $scope.counter = 0
+      $scope.$watch(scope => scope.someValue, (newValue, oldValue, scope) => {
+        scope.counter++
+      })
+
+      expect($scope.counter).toBe(0)
+
+      $scope.$digest()
+      expect($scope.counter).toBe(1)
+
+      $scope.$digest()
+      expect($scope.counter).toBe(1)
+
+      $scope.someValue = 'b'
+      expect($scope.counter).toBe(1)
+
+      $scope.$digest()
+      expect($scope.counter).toBe(2)
+    })
+
+    it('calls listener when watch value is first undefined', () => {
+      $scope.counter = 0
+
+      $scope.$watch(scope => scope.someValue, (newValue, oldValue, scope) => {
+        scope.counter++
+      })
+
+      $scope.$digest()
+      expect($scope.counter).toBe(1)
+    })
+
+    it('calls listener with new value as old value the first time', () => {
+      $scope.someValue = 123
+      let oldValueGiven
+
+      $scope.$watch(scope => scope.someValue, (newValue, oldValue) => {
+        oldValueGiven = oldValue
+      })
+
+      $scope.$digest()
+      expect(oldValueGiven).toBe(123)
+    })
+
+    it('may have watchers that omit the listener function', () => {
+      const watchFn = jasmine.createSpy().and.returnValue('something')
+      $scope.$watch(watchFn)
+
+      $scope.$digest()
+      expect(watchFn).toHaveBeenCalled()
     })
   })
 })
