85a4c906535ee4d482b18c7d9418c31dffa320fb
lijsh <sysulijs@gmail.com>
2016-08-09 15:29:07 +0800

    dirty checking by value

--- karma.conf.js
+++ karma.conf.js
@@ -1,22 +1,22 @@
 module.exports = function karmaConf(config) {
-  config.set({
-    frameworks: ['browserify', 'jasmine'],
-    files: [
-      'src/**/*.js',
-      'test/**/*_spec.js',
-    ],
-    preprocessors: {
-      'src/**/*.js': ['browserify'],
-      'test/**/*_spec.js': ['browserify'],
-    },
-    browsers: ['PhantomJS'],
-    browserify: {
-      debug: true,
-      configure(bundle) {
-        bundle.once('prebundle', () => {
-          bundle.transform('babelify', { presets: ['es2015'] })
-        })
-      },
-    },
-  })
+    config.set({
+        frameworks: ['browserify', 'jasmine'],
+        files: [
+            'src/**/*.js',
+            'test/**/*_spec.js',
+        ],
+        preprocessors: {
+            'src/**/*.js': ['browserify'],
+            'test/**/*_spec.js': ['browserify'],
+        },
+        browsers: ['PhantomJS'],
+        browserify: {
+            debug: true,
+            configure(bundle) {
+                bundle.once('prebundle', () => {
+                    bundle.transform('babelify', { presets: ['es2015'] })
+                })
+            },
+        },
+    })
 }
--- src/scope.js
+++ src/scope.js
@@ -3,46 +3,57 @@ import _ from 'lodash'
 function initWatchVal() {}
 
 module.exports = class Scope {
-  constructor() {
-    this.$$watchers = []
-    this.$$lastDirtyWatch = null
-  }
-  $watch(watchFn, listenerFn) {
-    const watcher = {
-      watchFn,
-      listenerFn: listenerFn || function () {},
-      last: initWatchVal,
+    constructor() {
+        this.$$watchers = []
+        this.$$lastDirtyWatch = null
     }
-    this.$$watchers.push(watcher)
-    this.$$lastDirtyWatch = null
-  }
-  $digest() {
-    let dirty
-    let ttl = 10
-    this.$$lastDirtyWatch = null
-    do {
-      dirty = this.$$digestOnce()
-      if (dirty && !(ttl--)) {
-        throw new Error('10 digest iteration reached')
-      }
-    } while (dirty)
-  }
-  $$digestOnce() {
-    let dirty
-    let newValue
-    let oldValue
-    _.forEach(this.$$watchers, watcher => {
-      newValue = watcher.watchFn(this)
-      oldValue = watcher.last
-      if (newValue !== oldValue) {
-        this.$$lastDirtyWatch = watcher
-        watcher.last = newValue
-        watcher.listenerFn(newValue, (oldValue === initWatchVal ? newValue : oldValue), this)
-        dirty = true
-      } else if (this.$$lastDirtyWatch === watcher) {
-        return false
-      }
-    })
-    return dirty
-  }
+    $watch(watchFn, listenerFn, valueEq) {
+        const watcher = {
+            watchFn,
+            listenerFn: listenerFn || function () {},
+            last: initWatchVal,
+            valueEq: !!valueEq,
+        }
+        this.$$watchers.push(watcher)
+        this.$$lastDirtyWatch = null
+    }
+    $digest() {
+        let dirty
+        let ttl = 10
+        this.$$lastDirtyWatch = null
+        do {
+            dirty = this.$$digestOnce()
+            if (dirty && !(ttl--)) {
+                throw new Error('10 digest iteration reached')
+            }
+        } while (dirty)
+    }
+    $$digestOnce() {
+        let dirty
+        let newValue
+        let oldValue
+        _.forEach(this.$$watchers, watcher => {
+            newValue = watcher.watchFn(this)
+            oldValue = watcher.last
+            if (!this.$$areEqual(newValue, oldValue, watcher.valueEq)) {
+                this.$$lastDirtyWatch = watcher
+                watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue)
+                watcher.listenerFn(newValue,
+                    (oldValue === initWatchVal ? newValue : oldValue),
+                    this)
+                dirty = true
+            } else if (this.$$lastDirtyWatch === watcher) {
+                return false
+            }
+        })
+        return dirty
+    }
+    $$areEqual(newValue, oldValue, valueEq) {
+        if (valueEq) {
+            return _.isEqual(newValue, oldValue)
+        }
+        return newValue === oldValue ||
+            (typeof newValue === 'number' && typeof oldValue === 'number' &&
+            isNaN(newValue) && isNaN(oldValue))
+    }
 }
